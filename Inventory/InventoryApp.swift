5d3ca10b672f4d2990265639f8a7d57b79683ccc
Mike <osxmichael@gmail.com>
2024-03-25 09:48:18 +0000

    Initial Commit

---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by Mike Garland on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
